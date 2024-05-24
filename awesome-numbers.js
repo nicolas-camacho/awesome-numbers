cbe250e6db85b579597c2b8862a0305b9724a53c
Nicolas Camacho Aguilar <nicolas.aguilar1999@gmail.com>
2024-05-24 00:54:54 -0500

    html added to test it

--- awesome-numbers.js
+++ awesome-numbers.js
@@ -38,3 +38,18 @@ const isPrime = (num) => {
  * @returns boolean
 */
 const isMultipleOf = (num, multiple) => num % multiple === 0;
+
+console.log("isEven", isEven(2)); // true
+console.log("isEven", isEven(3)); // false
+console.log("isOdd", isOdd(2)); // false
+console.log("isOdd", isOdd(3)); // true
+console.log("fibonacci", fibonacci(0)); // 0
+console.log("fibonacci", fibonacci(1)); // 1
+console.log("fibonacci", fibonacci(2)); // 1
+console.log("fibonacci", fibonacci(3)); // 2
+console.log("isPrime", isPrime(2)); // true
+console.log("isPrime", isPrime(3)); // true
+console.log("isPrime", isPrime(4)); // false
+console.log("isPrime", isPrime(5)); // true
+console.log("isMultipleOf", isMultipleOf(4, 2)); // true
+console.log("isMultipleOf", isMultipleOf(4, 3)); // false
