e609be0a0aaf4ec450ac06b4890c266673abad53
Nicolas Camacho Aguilar <nicolas.aguilar1999@gmail.com>
2024-05-28 16:30:55 -0500

    using ES5 classes

--- awesome-numbers.js
+++ awesome-numbers.js
@@ -1,40 +1,41 @@
-function Constructor(value) {
-  this.value = value;
-}
+class AwesomeNumber {
+  constructor(value) {
+    this.value = value;
+  }
+  /** 
+   * @returns boolean
+   */
+  isEven() {
+    return !(this.value & 1);
+  }
 
-/** 
- * @returns boolean
- */
-Constructor.prototype.isEven = function() {
-  return !(this.value & 1)
-};
+  /**
+   * @returns boolean
+   */
+  isOdd() {
+    return !this.isEven();
+  }
 
-/** 
- * @returns boolean
-*/
-Constructor.prototype.isOdd = function() {
-  return !this.isEven();
-} 
-
-/**
- * @returns boolean
- */
-Constructor.prototype.isPrime = function() {
+  /**
+   * @returns boolean
+   */
+  isPrime() {
     if (this.value <= 1) return false;
     if (this.value <= 3) return true;
     if (this.isEven()) return false;
     for (let i = 3; i <= Math.sqrt(this.value); i += 2) {
-        if (this.value % i === 0) return false;
+      if (this.value % i === 0) return false;
     }
     return true;
+  }
+
+  /** 
+   * @param {number} multiple
+   * @returns boolean
+  */
+  isMultipleOf(multiple) {
+    return this.value % multiple === 0;
+  }
 }
 
-/** 
- * @param {number} multiple
- * @returns boolean
-*/
-Constructor.prototype.isMultipleOf = function(multiple) {
-  return this.value % multiple === 0;
-} 
-
-export default Constructor;
+export default AwesomeNumber;
