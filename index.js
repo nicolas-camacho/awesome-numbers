783fc3b15cef7a83c5251433e78e3c280c244e36
Nicolas Camacho Aguilar <nicolas.aguilar1999@gmail.com>
2024-05-28 15:56:47 -0500

    turning into esmodules

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-console.log("isEven", awesomeNumbers.isEven(2)); // true
-console.log("isEven", awesomeNumbers.isEven(3)); // false
-console.log("isOdd", awesomeNumbers.isOdd(2)); // false
-console.log("isOdd", awesomeNumbers.isOdd(3)); // true
-console.log("fibonacci", awesomeNumbers.fibonacci(0)); // 0
-console.log("fibonacci", awesomeNumbers.fibonacci(1)); // 1
-console.log("fibonacci", awesomeNumbers.fibonacci(2)); // 1
-console.log("fibonacci", awesomeNumbers.fibonacci(3)); // 2
-console.log("isPrime", awesomeNumbers.isPrime(2)); // true
-console.log("isPrime", awesomeNumbers.isPrime(3)); // true
-console.log("isPrime", awesomeNumbers.isPrime(4)); // false
-console.log("isPrime", awesomeNumbers.isPrime(5)); // true
-console.log("isMultipleOf", awesomeNumbers.isMultipleOf(4, 2)); // true
-console.log("isMultipleOf", awesomeNumbers.isMultipleOf(4, 3)); // false
+import { isEven, isOdd, fibonacci, isPrime, isMultipleOf } from "./awesome-numbers.js";
+
+console.log("isEven", isEven(2)); // true
+console.log("isEven", isEven(3)); // false
+console.log("isOdd", isOdd(2)); // false
+console.log("isOdd", isOdd(3)); // true
+console.log("fibonacci", fibonacci(0)); // 0
+console.log("fibonacci", fibonacci(1)); // 1
+console.log("fibonacci", fibonacci(2)); // 1
+console.log("fibonacci", fibonacci(3)); // 2
+console.log("isPrime", isPrime(2)); // true
+console.log("isPrime", isPrime(3)); // true
+console.log("isPrime", isPrime(4)); // false
+console.log("isPrime", isPrime(5)); // true
+console.log("isMultipleOf", isMultipleOf(4, 2)); // true
+console.log("isMultipleOf", isMultipleOf(4, 3)); // false
