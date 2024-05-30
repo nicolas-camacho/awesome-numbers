b61ee2311212a615a53c0180cce15baf95481121
Nicolas Camacho <48541640+nicolas-camacho@users.noreply.github.com>
2024-05-30 12:23:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ console.log("isEven", awesomeNumbers.isEven(2)); // true
 console.log("isEven", awesomeNumbers.isEven(3)); // false
 console.log("isOdd", awesomeNumbers.isOdd(2)); // false
 console.log("isOdd", awesomeNumbers.isOdd(3)); // true
-console.log("fibonacci", awesomeNumbers.fibonacci(0)); // 0
-console.log("fibonacci", awesomeNumbers.fibonacci(1)); // 1
-console.log("fibonacci", awesomeNumbers.fibonacci(2)); // 1
-console.log("fibonacci", awesomeNumbers.fibonacci(3)); // 2
 console.log("isPrime", awesomeNumbers.isPrime(2)); // true
 console.log("isPrime", awesomeNumbers.isPrime(3)); // true
 console.log("isPrime", awesomeNumbers.isPrime(4)); // false
