9bc1b0b2af29c6dc51677bf663ab33d69f638f42
Nicolas Camacho <48541640+nicolas-camacho@users.noreply.github.com>
2024-05-30 12:23:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ console.log("isEven", isEven(2)); // true
 console.log("isEven", isEven(3)); // false
 console.log("isOdd", isOdd(2)); // false
 console.log("isOdd", isOdd(3)); // true
-console.log("fibonacci", fibonacci(0)); // 0
-console.log("fibonacci", fibonacci(1)); // 1
-console.log("fibonacci", fibonacci(2)); // 1
-console.log("fibonacci", fibonacci(3)); // 2
 console.log("isPrime", isPrime(2)); // true
 console.log("isPrime", isPrime(3)); // true
 console.log("isPrime", isPrime(4)); // false
