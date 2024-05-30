9d68aebc95b4cf7d8c879e307671f537541f953c
Nicolas Camacho <48541640+nicolas-camacho@users.noreply.github.com>
2024-05-30 12:24:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-import { isEven, isOdd, fibonacci, isPrime, isMultipleOf } from "./awesome-numbers.js";
+import { isEven, isOdd, isPrime, isMultipleOf } from "./awesome-numbers.js";
 
 console.log("isEven", isEven(2)); // true
 console.log("isEven", isEven(3)); // false
 console.log("isOdd", isOdd(2)); // false
 console.log("isOdd", isOdd(3)); // true
-console.log("fibonacci", fibonacci(0)); // 0
-console.log("fibonacci", fibonacci(1)); // 1
-console.log("fibonacci", fibonacci(2)); // 1
-console.log("fibonacci", fibonacci(3)); // 2
 console.log("isPrime", isPrime(2)); // true
 console.log("isPrime", isPrime(3)); // true
 console.log("isPrime", isPrime(4)); // false
