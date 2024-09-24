7d5c9afff1393ceaa325748c488bb1b96033a675
Nicolas Camacho <ncamacho@twilio.com>
2024-09-24 01:43:39 -0500

    ci/cd: testing and build

--- index.js
+++ index.js
@@ -9,3 +9,4 @@ console.log("isOdd", myAwesomeNumber.isOdd()); // false
 console.log("isPrime", myAwesomeNumber.isPrime()); // true
 console.log("isMultipleOf 2", myAwesomeNumber.isMultipleOf(2)); // true
 console.log("isMultipleOf 3", myAwesomeNumber.isMultipleOf(3)); // false
+console.log("sum with raw number", myAwesomeNumber + 2); // 6
---
+++ test/unitTest.test.js
@@ -0,0 +1,30 @@
+import AwesomeNumber from "../awesome-numbers";
+
+describe("AwesomeNumber", () => {
+    const myAwesomeNumber = new AwesomeNumber(4);
+
+    it("isEven", () => {
+        expect(myAwesomeNumber.isEven()).toBe(true);
+    });
+
+    it("isOdd", () => {
+        expect(myAwesomeNumber.isOdd()).toBe(false);
+    });
+
+    it("isPrime", () => {
+        expect(myAwesomeNumber.isPrime()).toBe(false);
+    });
+
+    it("isMultipleOf 2", () => {
+        expect(myAwesomeNumber.isMultipleOf(2)).toBe(true);
+    });
+
+    it("isMultipleOf 3", () => {
+        expect(myAwesomeNumber.isMultipleOf(3)).toBe(false);
+    });
+
+    it("sum with raw number", () => {
+        let newNumber = myAwesomeNumber + 2;
+        expect(newNumber).toBe(6);
+    });
+});
