c2f4f095e84c6fbb9185d077d545c5cc3639408c
Nicolas Camacho <ncamacho@twilio.com>
2024-09-24 03:18:14 -0500

    new: example

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ console.log("isPrime", myAwesomeNumber.isPrime()); // true
 console.log("isMultipleOf 2", myAwesomeNumber.isMultipleOf(2)); // true
 console.log("isMultipleOf 3", myAwesomeNumber.isMultipleOf(3)); // false
 console.log("sum with raw number", myAwesomeNumber + 2); // 6
+
+const newNumber = myAwesomeNumber + new AwesomeNumber(2);
+
+console.log("sum with AwesomeNumber", newNumber + myAwesomeNumber); // 6
